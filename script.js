4f79ef92a16a6b70fb2609ffa27157767f2c6415
Dimas Maulana <dimashakim953@gmail.com>
2026-04-22 13:24:15 +0700

    update tampilan

--- script.js
+++ script.js
@@ -5,22 +5,22 @@ const books = [
     {
         title: "Laut Bercerita",
         price: 89000,
-        image: "https://via.placeholder.com/150"
+        image: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcREVzCF2rOGKb2emwjoFs9JtyEKNK9uX-fDjTpu3FAl0b0fmng7XwzVELJ6soBzle_YLibolIwiagkJx83p3Zkb1qSCdk7G3rsqRhKPD6tE60T9cECySynh"
     },
     {
         title: "Bumi",
         price: 79000,
-        image: "https://via.placeholder.com/150"
+        image: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRiO-yAEjNSE7rh3-2_FKDGSt4xIlb3Lj0swnr4WO3TMNZYxb71JRu39lPYKCQrCQEoO1Eerb60hP68kXpdr3vHRjSPIaF4MtEkTsEJlmg_i7e-xvmEBY8Xs1Y"
     },
     {
         title: "Sepotong Hati",
         price: 85000,
-        image: "https://via.placeholder.com/150"
+        image: "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTOovs7d8zV-vpGRxNrRucLKOT7g0O6jpUX6aLSElYOgNp86-KwnfDQSLshzzGzgVDTG-h6r1QGVb-qckDchTM7Yr0TnGDaq4xvYKjV1eUaqdLgHqadm89U"
     },
     {
         title: "Atomic Habits",
         price: 99000,
-        image: "https://via.placeholder.com/150"
+        image: "https://down-id.img.susercontent.com/file/id-11134207-8224y-mi8duala5ukgd8"
     }
 ];
 
